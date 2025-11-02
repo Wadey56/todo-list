474f3913622c71dd7a7b815c1d4856b8da18746b
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-11-02 22:28:22 +0000

    Add edit module based on an array, id and the edit

---
+++ src/editTask.js
@@ -0,0 +1,16 @@
+// this module is used to edit tasks
+
+// function to delete task passed in
+function editTask(taskList, id, attribte, value) {
+
+    let taskIndex = taskList.findIndex(task => task.id === id);
+    let task = taskList[taskIndex];
+    // bracket notation required to dynamic key value
+    task[attribte] = value;
+
+    taskList.filter((task) => task.id !== id);
+
+    return taskList;
+}
+
+export { editTask };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { createTask } from "./createTask.js"
 import { deleteTask } from "./deleteTask.js"
+import { editTask } from "./editTask.js"
 
 // test create
 const task1 = createTask("task 1", "description 1", "2025-01-01", "high", "project 1");
+const task2 = createTask("task 2", "description 2", "2025-01-01", "high", "project 1");
 
 // temp storage
-export let tempStorage = [task1];
+export let tempStorage = [task1, task2];
+task1.description = "edited description";
 console.log(tempStorage);
 
 // test delete
 tempStorage = deleteTask(tempStorage, task1.id);
+console.log(tempStorage);
+
+// test edit
+tempStorage = editTask(tempStorage, task2.id, "title", "new title");
 console.log(tempStorage);
